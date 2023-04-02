82a5c202db556b9a71e9e0d0aa4f4f92328a7701
MarekPuu <78322919+MarekPuu@users.noreply.github.com>
2023-04-02 12:33:38 +0300

    Google Analytics lisätty

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -1,8 +1,15 @@
 import './Layout.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import { useEffect } from 'react';
+import { logPageView } from '../../utils/analytics';
 
 const DefaultLayout = () => {
+  const location = useLocation();
+  useEffect(() => {
+    logPageView();
+  }, [location]);
+
   return (
     <div className="default_layout_container">
       <div className="default_layout_outlet">
